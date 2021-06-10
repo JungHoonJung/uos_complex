c74873e11117143a86bb88577915b5f7b73bbdef
junghoon.jung <56838564+JungHoonJung@users.noreply.github.com>
2021-06-10 22:32:07 +0900

    req

--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
 from setuptools import setup, find_packages
 from uos_complex import __version__
-#from pip.req import parse_requirements
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-    from pip.download import PipSession
+
 ################################################################################
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("requirements.txt", session=PipSession())
-reqs = [str(ir.req) for ir in install_reqs]
+with open("requirements.txt") as f:
+	reqs = f.read().split()
 
 
 setup(name='uos_complex',
