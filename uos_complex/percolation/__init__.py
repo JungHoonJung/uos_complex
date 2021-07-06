121ce103aba3fb2dcdf4cacf6ec0222dbda85700
junghoon.jung <56838564+JungHoonJung@users.noreply.github.com>
2021-07-06 17:11:51 +0900

    percol correction

--- uos_complex/percolation/__init__.py
+++ uos_complex/percolation/__init__.py
@@ -1,12 +1,10 @@
 import networkx as nx
-from networkx.algorithms.components import strongly_connected
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.lib.arraysetops import isin
 
 
 
-def edge_weight_percolation(network : nx.Graph, weight_name : str) -> np.ndarray:
+def edge_weight_percolation(network : nx.Graph, weight_name : str, connecting = 'strong') -> np.ndarray:
     '''Percolation with given weight on given network.
     Parameters
     ---------------
@@ -18,7 +16,7 @@ def edge_weight_percolation(network : nx.Graph, weight_name : str) -> np.ndarray
     Return
     ---------------
     `np.ndarray` or `list`
-        From each node as each cluster, the return is a merge ordered list of edges and its cluster.
+        From each node as each cluster, the return is a merging ordered list of edges and its cluster.
         The form of each element is ('weight', startnode, endnode, cluster1, cluster2).
         If cluster is negative, it means there is no merging of clusters between such a edge.
     
@@ -41,28 +39,41 @@ def edge_weight_percolation(network : nx.Graph, weight_name : str) -> np.ndarray
     Finally, edge (2,3) was merged. Current state : 1, 1, 1, 1 (cluster 1 contains all nodes.).
     '''
 
-    directional = False if isinstance(network, nx.Graph) else True
+    strong = True if connecting == 'strong' else False
 
-    if directional and not isinstance(network, nx.DiGraph):
-        raise ValueError("'network' must be a instance of networkx graph.")
-    edges = np.array([[wt,u,v]for (u,v, wt) in network.edges.data(weight_name)])   # container for sorting
+    if not isinstance(network, nx.DiGraph) and not isinstance(network, nx.Graph):
+        raise ValueError("'network' must be a instance of networkx `DiGraph` or `Graph`.")
+    
+    if strong and not isinstance(network, nx.DiGraph):
+        strong = False
+        raise Warning("Undirected graph cannot be operated on strongly connected component. Weakly connected component will be calculated.")
+
+    edges = np.array([[wt,u,v] for (u,v, wt) in network.edges.data(weight_name)])   # container for sorting
     order = np.argsort(edges[:,0])                                                  # sort by weight
 
     class cluster:
         def __init__(self, data):
-            self.head = self    # the representative element of cluster
+            self.head = None    # the representative element of cluster
             self.data = data    # the representative data of cluster
+
+        def leader(self):
+            current = self
+            while current.head is not None:
+                #print(current.data)
+                current = current.head
+            return current
         
         def merge(self, other):
-            other.head = self.head  #only exchange the representative element
-
+            target, cl = (self, other) if self>other else (other, self)
+            target.leader().head = cl.leader()
+            
         def __gt__(self, other):
-            return self.head.data > other.head.data # for deciding which is more representative
+            return self.leader().data > other.leader().data # for deciding which is more representative
 
     #make empty Graph
     percolation = []
     percolnet = network.__class__()
-    clusters = {i:cluster(i) for i in range(len(network.nodes))} # some python tricks which is python list act like a pointer of C.
+    clusters = {i:cluster(i) for i in range(len(network.nodes))} # initial cluster state is that each node is a cluster itself.
     for o in order:
         weight, st_node, end_node = edges[o]                # weight, start node, end node, respectively.
         cl1 , cl2 = clusters[st_node], clusters[end_node]   # cl1 and cl2 are cluster1 and cluster2, respectively.
@@ -70,23 +81,19 @@ def edge_weight_percolation(network : nx.Graph, weight_name : str) -> np.ndarray
         percolnet.add_edge(st_node, end_node)               # Make percolation by adding new edge by edge.
 
         merged = False
-        if directional:
-            ## checking strongly connencted component clustering #################################### YG code here, if merge occurs, then merged = True
-            pass
-            
-
-        else:
-            ## checking weakly connencted component clustering   #################################### YG code here, if merge occurs, then merged = True
+        if strong:
+            ## checking strongly connencted  #################################### YG code here, if merge occurs, then merged = True
             pass
         
-        if merged:
-            percolation.append(weight, st_node, end_node, cl1.head.data, cl2.head.data)
-            if cl1< cl2:
-                cl1.merge(cl2)
-            else:
-                cl2.merge(cl1)
         else:
-            percolation.append(weight, st_node, end_node, -1, -1)
+            ## checking weakly connencted    #################################### YG code here, if merge occurs, then merged = True
+            pass
+        
+        if merged and cl1 != cl2:
+            percolation.append([weight, st_node, end_node, cl1.head.data, cl2.head.data])
+            cl1.merge(cl2)
+        else:
+            percolation.append([weight, st_node, end_node, -1, -1])
     
     return percolation
     
